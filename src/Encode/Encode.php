748d09b7769c62d1f7ddeb2a7b36bb5bc5b68915
Ferran Martinez <alfonsmartinezparra@gmail.com>
2016-04-28 15:39:20 +0200

    initial commit

---
+++ src/Encode/Encode.php
@@ -0,0 +1,227 @@
+<?php
+
+namespace Buuum\Encoding;
+
+class Encode
+{
+
+    /**
+     * @var
+     */
+    public static $key;
+
+    /**
+     * @var string
+     */
+    private static $alg = 'GOST';
+
+    /**
+     * @var array
+     */
+    private static $supported_algs = [
+        'RIJNDAELE' => [MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB, 32],
+        'RIJNDAELC' => [MCRYPT_RIJNDAEL_256, MCRYPT_MODE_CBC, 32],
+        'BLOWFISH'  => [MCRYPT_BLOWFISH, MCRYPT_MODE_ECB, false],
+        '3DES'      => [MCRYPT_3DES, MCRYPT_MODE_ECB, 24],
+        'GOST'      => [MCRYPT_GOST, MCRYPT_MODE_CBC, 32]
+    ];
+
+    /**
+     * @param array $data
+     * @param array|null $head
+     * @return string
+     */
+    public static function encode(array $data, array $head = null)
+    {
+        $header = [
+            'expires' => 0,
+            'delay'   => 0
+        ];
+        if (isset($head) && is_array($head)) {
+            $header = array_merge($header, $head);
+        }
+
+        if ($header['expires'] > 0) {
+            $header['expires'] += time();
+        }
+        if ($header['delay'] > 0) {
+            $header['delay'] += time();
+        }
+
+        $segments = [];
+        $segments[] = self::jsonEncode($header);
+        $segments[] = self::jsonEncode($data);
+
+        return self::sign(implode('.', $segments));
+    }
+
+    /**
+     * @param $data
+     * @return string
+     */
+    private static function sign($data)
+    {
+        list($cipher, $mode, $size) = self::$supported_algs[self::$alg];
+
+        $iv_size = mcrypt_get_iv_size($cipher, $mode);
+        $iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
+
+        return self::base64_url_encode($iv . mcrypt_encrypt(
+                $cipher,
+                self::getKey($size), $data,
+                $mode,
+                $iv
+            ));
+    }
+
+    /**
+     * @param $size
+     * @return string
+     */
+    private static function getKey($size)
+    {
+        $key = sha1(self::$key);
+        if ($size) {
+            $key = substr($key, 0, $size);
+        }
+
+        return $key;
+    }
+
+    /**
+     * @param $alg
+     */
+    public static function setAlgorithm($alg)
+    {
+        if (empty(self::$supported_algs[$alg])) {
+            throw new \DomainException('Algorithm not supported');
+        }
+
+        self::$alg = $alg;
+    }
+
+    /**
+     * @param $data
+     * @return mixed
+     * @throws \Exception
+     */
+    static public function decode($data)
+    {
+        list($cipher, $mode, $size) = self::$supported_algs[self::$alg];
+
+        $data = self::base64_url_decode($data);
+        $iv_size = mcrypt_get_iv_size($cipher, $mode);
+        $iv_dec = substr($data, 0, $iv_size);
+        $data = substr($data, $iv_size);
+
+
+        $datainfo = trim(mcrypt_decrypt(
+            $cipher,
+            self::getKey($size),
+            $data,
+            $mode,
+            $iv_dec
+        ));
+        $segments = explode('.', $datainfo, 2);
+
+        $info = self::jsonDecode($segments[0]);
+
+        if ($info['expires'] > 0 && $info['expires'] < time()) {
+            throw new \Exception(
+                'This token expired on ' . date(\DateTime::ISO8601, $info['expires'])
+            );
+        }
+        if ($info['delay'] > 0 && $info['delay'] > time()) {
+            throw new \Exception(
+                'Cannot handle token prior to ' . date(\DateTime::ISO8601, $info['delay'])
+            );
+        }
+
+        return self::jsonDecode($segments[1]);
+    }
+
+    /**
+     * @param array $input
+     * @return string
+     */
+    private static function jsonEncode(array $input)
+    {
+        $json = json_encode($input);
+        if (function_exists('json_last_error') && $errno = json_last_error()) {
+            self::handleJsonError($errno);
+        } elseif ($json === 'null' && $input !== null) {
+            throw new \DomainException('Null result with non-null input');
+        }
+        return $json;
+    }
+
+    /**
+     * @param $input
+     * @return mixed
+     */
+    private static function jsonDecode($input)
+    {
+        if (version_compare(PHP_VERSION, '5.4.0', '>=') && !(defined('JSON_C_VERSION') && PHP_INT_SIZE > 4)) {
+            /** In PHP >=5.4.0, json_decode() accepts an options parameter, that allows you
+             * to specify that large ints (like Steam Transaction IDs) should be treated as
+             * strings, rather than the PHP default behaviour of converting them to floats.
+             */
+            $obj = json_decode($input, true, 512, JSON_BIGINT_AS_STRING);
+        } else {
+            /** Not all servers will support that, however, so for older versions we must
+             * manually detect large ints in the JSON string and quote them (thus converting
+             *them to strings) before decoding, hence the preg_replace() call.
+             */
+            $max_int_length = strlen((string)PHP_INT_MAX) - 1;
+            $json_without_bigints = preg_replace('/:\s*(-?\d{' . $max_int_length . ',})/', ': "$1"', $input);
+            $obj = json_decode($json_without_bigints, true);
+        }
+        if (function_exists('json_last_error') && $errno = json_last_error()) {
+            self::handleJsonError($errno);
+        } elseif ($obj === null && $input !== 'null') {
+            throw new \DomainException('Null result with non-null input');
+        }
+        return $obj;
+    }
+
+
+    /**
+     * @param $errno
+     */
+    private static function handleJsonError($errno)
+    {
+        $messages = array(
+            JSON_ERROR_DEPTH     => 'Maximum stack depth exceeded',
+            JSON_ERROR_CTRL_CHAR => 'Unexpected control character found',
+            JSON_ERROR_SYNTAX    => 'Syntax error, malformed JSON'
+        );
+        throw new \DomainException(
+            isset($messages[$errno])
+                ? $messages[$errno]
+                : 'Unknown JSON error: ' . $errno
+        );
+    }
+
+    /**
+     *
+     * @param $input
+     * @return string
+     *
+     */
+    private static function base64_url_encode($input)
+    {
+        return strtr(base64_encode($input), '+/', '-_');
+    }
+
+    /**
+     *
+     * @param $input
+     * @return string
+     *
+     */
+    private static function base64_url_decode($input)
+    {
+        return base64_decode(strtr($input, '-_', '+/'));
+    }
+
+}
---
+++ tests/EncodeTest.php
@@ -0,0 +1,120 @@
+<?php
+
+class EncodeTest extends PHPUnit_Framework_TestCase
+{
+
+
+    public static $data = [
+        'key1'   => 'value 1',
+        'key2'   => 'value 2',
+        'newkey' => 'Key ??? %$·"!!"·$%&'
+    ];
+
+    /**
+     * Sets up the fixture, for example, opens a network connection.
+     * This method is called before a test is executed.
+     */
+    protected function setUp()
+    {
+    }
+
+    /**
+     * Tears down the fixture, for example, closes a network connection.
+     * This method is called after a test is executed.
+     */
+    protected function tearDown()
+    {
+    }
+
+    public static function setUpBeforeClass()
+    {
+
+    }
+
+    public function testEncode()
+    {
+        \Buuum\Encoding\Encode::$key = 'My Secret Key';
+        $code = \Buuum\Encoding\Encode::encode(self::$data);
+        $this->assertEquals(\Buuum\Encoding\Encode::decode($code), self::$data);
+    }
+
+    /**
+     * @dataProvider getAlgorithms
+     */
+    public function testEncodeByType($alg)
+    {
+        \Buuum\Encoding\Encode::$key = 'My Secret Key';
+        \Buuum\Encoding\Encode::setAlgorithm($alg);
+        $code = \Buuum\Encoding\Encode::encode(self::$data);
+        $this->assertEquals(\Buuum\Encoding\Encode::decode($code), self::$data);
+    }
+
+    /**
+     * @dataProvider getAlgorithms
+     * @expectedException Exception
+     */
+    public function testEncodeDelayKo($alg)
+    {
+        \Buuum\Encoding\Encode::$key = 'My Secret Key';
+        \Buuum\Encoding\Encode::setAlgorithm($alg);
+        $code = \Buuum\Encoding\Encode::encode(self::$data, ['delay' => 2]);
+        \Buuum\Encoding\Encode::decode($code);
+    }
+
+    /**
+     * @dataProvider getAlgorithms
+     * @expectedException Exception
+     */
+    public function testEncodeExpiresKo($alg)
+    {
+        \Buuum\Encoding\Encode::$key = 'Another !! Secret Key';
+        \Buuum\Encoding\Encode::setAlgorithm($alg);
+        $code = \Buuum\Encoding\Encode::encode(self::$data, ['expires' => 1]);
+        sleep(2);
+        \Buuum\Encoding\Encode::decode($code);
+    }
+
+    /**
+     * @dataProvider getAlgorithms
+     */
+    public function testEncodeDelayOk($alg)
+    {
+        \Buuum\Encoding\Encode::$key = 'My new Secret Key';
+        \Buuum\Encoding\Encode::setAlgorithm($alg);
+        $code = \Buuum\Encoding\Encode::encode(self::$data, ['delay' => 1]);
+        sleep(2);
+        $this->assertEquals(\Buuum\Encoding\Encode::decode($code), self::$data);
+    }
+
+    /**
+     * @dataProvider getAlgorithms
+     */
+    public function testExpiresOK($alg)
+    {
+        \Buuum\Encoding\Encode::$key = 'My ////new Secret Key ??? %$·"!!"·$%&';
+        \Buuum\Encoding\Encode::setAlgorithm($alg);
+        $code = \Buuum\Encoding\Encode::encode(self::$data, ['expires' => 10]);
+        $this->assertEquals(\Buuum\Encoding\Encode::decode($code), self::$data);
+    }
+
+    public function getAlgorithms()
+    {
+        return [
+            ['RIJNDAELE'],
+            ['RIJNDAELC'],
+            ['BLOWFISH'],
+            ['3DES'],
+            ['GOST']
+        ];
+    }
+
+    ///**
+    // * @expectedException \Buuum\Exception\HttpRouteNotFoundException
+    // */
+    //public function testExceptionHttpRouteNotFound()
+    //{
+    //    $dispatcher = new \Buuum\Dispatcher(self::$router->getData());
+    //    $dispatcher->dispatchRequest('POST', '/items/11');
+    //}
+
+}
